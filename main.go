e3a4f824b28b568021389aa8b7c16e005af8f969
xHozey <hamzasaliheddine@hotmail.com>
2024-05-06 17:43:06 +0100

    v2.1

--- main.go
+++ main.go
@@ -20,15 +20,13 @@ func main() {
 	input := os.Args[1]
 
 	text := strings.Split(input, "\\n")
-
 	fileres := map[rune][]string{}
-
 	for i, val := range file {
 		fileres[rune(i+32)] = strings.Split(val, "\n")
 	}
+
 	for _, val := range text {
 		if val == "" {
-			fmt.Println()
 			continue
 		}
 		for i := 0; i < 8; i++ {
