e8eda0cd633cc4e6f5f332fd8453ee057c8789cb
hamza saliheddine <hamzasaliheddine@hotmail.com>
2024-05-07 12:12:51 +0100

    v2.5

--- main.go
+++ main.go
@@ -7,33 +7,43 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Invalid args")
-		return
-	}
 	data, err := os.ReadFile("standard.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 		return
 	}
-	file := strings.Split(string(data[1:]), "\n\n")
+	standard := strings.Split(string(data), "\n\n")
 	input := os.Args[1]
+	splitInput := strings.Split(input, "\\n")
+	var match bool
 
-	text := strings.Split(input, "\\n")
-	fileres := map[rune][]string{}
-	for i, val := range file {
-		fileres[rune(i+32)] = strings.Split(val, "\n")
+	for _, i := range splitInput {
+		if i != "" {
+			match = true
+			break
+		}
 	}
-
-	for _, val := range text {
+	// "" "k" "" ""
+	matrix := map[rune][]string{}
+	for i, val := range standard {
+		matrix[rune(32+i)] = strings.Split(val, "\n")
+	}
+	for i, val := range splitInput {
 		if val == "" {
-			continue
-		}
-		for i := 0; i < 8; i++ {
-			for _, val2 := range val {
-				fmt.Print(fileres[val2][i])
+			if match {
+				fmt.Println()
+			} else if i != 0 && !match {
+				fmt.Println()
 			}
-			fmt.Println()
+
+		} else if val != "" {
+			for j := 0; j < 8; j++ {
+				for _, k := range val {
+					fmt.Print(matrix[k][j])
+				}
+				fmt.Println()
+			}
 		}
 	}
+
 }
